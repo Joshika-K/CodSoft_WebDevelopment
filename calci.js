d8f1d4752bbe84928101bcb030145a37bb4f4eb6
Joshika-K <joshikalyan03@gmail.com>
2024-06-03 03:04:03 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendNumber(number) {
+    if (display.innerText === '0') {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function appendSymbol(symbol) {
+    display.innerText += symbol;
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
